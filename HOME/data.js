6937b34508db5f14411a33f05564387422641378
Ciaoen <160987124+Ciaoen@users.noreply.github.com>
2024-04-19 11:42:40 +0800

    pano

---
+++ HOME/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-10000",
+      "name": "10000門市",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6967591813039755,
+        "pitch": -0.06245561338272054,
+        "fov": 0.6002044175770411
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.3936746469816423,
+          "pitch": -0.10151430920601356,
+          "title": "10000門市",
+          "text": "亞洲第10000間小七"
+        }
+      ]
+    }
+  ],
+  "name": "10000門市",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ 航海王超商/data.js
@@ -0,0 +1,240 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "航空門市",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.4980605735726442,
+        "pitch": 0.007883785807651478,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.382344840581995,
+          "pitch": 0.10567055391159386,
+          "rotation": 4.71238898038469,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.5597851312288213,
+          "pitch": 0.38867990711498557,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.9358659883011837,
+          "pitch": 0.09874224640621776,
+          "rotation": 1.5707963267948966,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1252282533980988,
+          "pitch": 0.4784557835447565,
+          "title": "魯夫",
+          "text": "航海王主角之一-魯夫"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "航空右",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.03679803191919,
+        "pitch": -0.025291893900321938,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.179618691206855,
+          "pitch": -0.059010411548664266,
+          "rotation": 3.9269908169872414,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "航空外",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.385577712297561,
+        "pitch": -0.10622595438136173,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4281287921410417,
+          "pitch": 0.2821558573072238,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5409849445226849,
+          "pitch": 0.08756681252025089,
+          "title": "超商外觀",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "航空左",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.7133869850788903,
+        "pitch": -0.07587568170097825,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1228469863082724,
+          "pitch": 0.1785457942327433,
+          "rotation": 7.0685834705770345,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.1530456095995003,
+          "pitch": 0.1721412258038555,
+          "rotation": 4.71238898038469,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "航空左左",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6191706432425503,
+        "pitch": 0.07840487109099925,
+        "fov": 1.2828048894984105
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.650766209148033,
+          "pitch": 0.20416949051203304,
+          "rotation": 1.5707963267948966,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2153542193587423,
+          "pitch": 0.3580182375908336,
+          "title": "公車站牌",
+          "text": "緊鄰公車站牌，交通便利"
+        }
+      ]
+    }
+  ],
+  "name": "航海王超商",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
